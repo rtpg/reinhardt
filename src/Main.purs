3f1a43e0fb8ba190fac6ee15f206ebb17ebc2c4b
Raphael Gaschignard <raphael@rtpg.co>
2016-07-23 01:32:18 +0900

    move to purescript 0.9.1

--- src/Main.purs
+++ src/Main.purs
@@ -1,15 +1,13 @@
 module Main where
 
-import Node.Express.App
 import Control.Monad.Eff.Console as Console
 import Node.Process as Process
 import Data.Array (slice, length)
 import Data.Traversable (traverse)
-import Node.Express.Response (sendJson)
-import Prelude (return, ($), bind)
+import Prelude (pure, ($), bind)
 
 main = do
   args <- Process.argv
   Console.log "Hi guys"
   traverse Console.log (slice 2 (length args) args)
-  return 0
+  pure 0
